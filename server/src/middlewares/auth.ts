ab947f25bd4887d6dd146b7b05a9f94842259ee2
Silas Leite <bigchill_silas@hotmail.com>
2023-10-15 18:15:38 -0300

    :sparkles: feat: Middleware for authentication

---
+++ server/src/middlewares/auth.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { jwtAuthenticate, JwtError } from '../config/jwt';
+import GetUserService  from '../domain/modules/user/services/get/GetUserService';
+
+async function ensureAuthenticated(
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<void> {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) {
+    reply.unauthorized(null, '');
+  }
+
+  try {
+    const { user } = jwtAuthenticate(authToken!);
+
+    const authenticatedUser = await GetUserService.execute([
+      { $project: { email: 1 } },
+      { $match: { email: user.email.getEmail() } },
+    ]);
+
+    if (!authenticatedUser) {
+      reply.unauthorized(null, '');
+    }
+  } catch (error) {
+    const err = error as JwtError;
+    const message = err.expiredAt ? `${err.message}: ${err.expiredAt}` : err.message;
+    reply.unauthorized(message, '');
+  }
+}
+
+export default ensureAuthenticated;
+
